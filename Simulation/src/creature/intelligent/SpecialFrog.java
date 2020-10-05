6e67de11ffe5477230736b21e93fff8ffff49fd5
= <=>
2020-10-05 18:25:14 +0330

    frog is evaluated!

---
+++ Simulation/src/creature/intelligent/SpecialFrog.java
@@ -0,0 +1,34 @@
+package creature.intelligent;
+
+import creature.base.actions.Jump;
+
+import java.util.Scanner;
+
+public class SpecialFrog extends IntelligentStuff implements Jump
+{
+
+    @Override
+    public void jump() {
+
+    }
+
+    @Override
+    public void learn() {
+
+    }
+
+    @Override
+    public void start(Scanner scanner) {
+
+    }
+
+    @Override
+    protected void evaluate() {
+
+    }
+
+    @Override
+    protected void eat() {
+
+    }
+}
