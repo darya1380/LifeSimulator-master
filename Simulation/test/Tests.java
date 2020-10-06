a5eb6e085da33463b892800c8d4d97ec7a786008
= <=>
2020-10-06 07:40:28 +0330

    final

--- Simulation/test/Tests.java
+++ Simulation/test/Tests.java
@@ -1,10 +1,10 @@
 import creature.base.animals_basics.FlyingCreature;
 import org.junit.Test;
 import static org.junit.Assert.*;
-//todo i don't know why are we using this tests
-//todo i mean we have main method and it, works actually!
-//todo and for sure my methods return void so there is no point in writing tests!
-//todo is junit really useful while there is no TDD?
+// i don't know why are we using this tests
+// i mean we have main method and it, works actually!
+// and for sure my methods return void so there is no point in writing tests!
+// is junit really useful while there is no TDD?
 public class Tests
 {
     @Test
